a1bd17995d1bff3062250c9fe6508758ea346edf
Rémy Dupont <remy.dupont2002@gmail.com>
2024-09-20 20:20:52 +0200

    bonjour

--- Particules_mieux_ppar.c
+++ Particules_mieux_ppar.c
@@ -2,8 +2,8 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-#define delta_t  10e-4
-#define nbx_particules   2
+#define delta_t  10e-6
+#define nbx_particules   5
 
 /*Definition Particule avec x,y,vitesse selon x
 , vitesse selon y*/
@@ -13,6 +13,8 @@ typedef struct
     double y;
     double vx;
     double vy;
+    double fx;
+    double fy;
 } Particule;
 
 /*Constructeur*/
@@ -25,7 +27,7 @@ void initPoint(Particule *p, double x, double y, double vx, double vy)
 }
 /*Calcul distance entre 2 particules*/
 double distance(Particule *p1, Particule *p2) {
-    return sqrt(pow(p1->x - p2->x, 2) + pow(p1->y - p2->y, 2));
+    return sqrt(pow(p2->x - p1->x, 2) + pow(p2->y - p1->y, 2));
 }
 
 /*Fonction qui genere des positions et vitesse 
@@ -42,23 +44,23 @@ Particule genererParticule()
 
 /*Fonction qui calcule la force selon x en Lennard Jones entre 
 deux particules*/
-double force_selon_x(Particule *p1, Particule *p2)
+double force_selon_x(Particule *p2, Particule *p1)
 {
     double rij = distance(p1, p2);
-    double terme_commun = (24/pow(rij,2))*pow((1/rij),6)*(2*pow((1/rij),6)-1);
+    double terme_commun = 24*(pow(1/rij,8)-2*pow(1/rij,14));
     return terme_commun*(p2->x-p1->x);
 }
 
 /*Fonction qui calcule la force selon y en Lennard Jones entre 
 deux particules*/
-double force_selon_y(Particule *p1, Particule *p2)
+double force_selon_y(Particule *p2, Particule *p1)
 { 
     double rij = distance(p1, p2);
-    double terme_commun = (24/pow(rij,2))*pow((1/rij),6)*(2*pow((1/rij),6)-1);
+    double terme_commun = 24*(pow(1/rij,8)-2*pow(1/rij,14));
     return terme_commun*(p2->y-p1->y);
 }
 
-double Potentiel_LJ(Particule *p1, Particule *p2)
+double Potentiel_LJ(Particule *p2, Particule *p1)
 {
     double rij=distance(p1,p2);
     double U=4*(pow((1/rij),12)-pow((1/rij),6));
@@ -66,14 +68,20 @@ double Potentiel_LJ(Particule *p1, Particule *p2)
 }
 
 double Energie_totale(Particule tab_par[])
-{
-    double E=0,U=0;
-    for (int i=0;i<nbx_particules-1;i++)
+{   
+    double E=0.0,U=0.0;
+
+    for (int i=0;i<nbx_particules;i++)
     { 
-        U+=Potentiel_LJ(&tab_par[i],&tab_par[i+1]);
-        E+=(pow(tab_par[i].vx,2)+pow(tab_par[i].vy,2))/2 ;
+        for (int j=i+1;j<nbx_particules;j++)
+        { 
+        
+            U+=Potentiel_LJ(&tab_par[j],&tab_par[i]);
+        
+        }
+        E+=(pow(tab_par[i].vx,2)+pow(tab_par[i].vy,2))/2;
+
     }
-    E+=(pow(tab_par[nbx_particules-1].vx,2)+pow(tab_par[nbx_particules-1].vy,2))/2 ;
     return U+E;
 }
 
@@ -81,25 +89,48 @@ double Energie_totale(Particule tab_par[])
 designé par un "indice", et qui prend un tableau dans lequel sont 
 stockées les particules, elle ne retourne rien car tout est modifié 
 directement */
-void resolution(Particule tab_par[], int indice)
+void calcul_forces (Particule tab_par[])
 {
     //je commence par déterminer f_indice_x :
-    double f_x =0;
-    double f_y =0;
+    double f_x;
+    double f_y;
 
     for ( int i=0;i<nbx_particules;i++)
     {
-        if (indice != i)
-        { 
-            f_x += force_selon_x(&tab_par[indice],&tab_par[i]);
-            f_y += force_selon_y(&tab_par[indice],&tab_par[i]);
+        f_x=0.0;
+        f_y=0.0;
+        for ( int j=0;j<nbx_particules;j++)
+        {
+            if (j != i)
+            { 
+                f_x += force_selon_x(&tab_par[j],&tab_par[i]);
+                f_y += force_selon_y(&tab_par[j],&tab_par[i]);
+            }
         }
+
+        tab_par[i].fx=f_x;
+        tab_par[i].fy=f_y;
+    }
+    
+}  
+
+void resolution(Particule tab_par[])
+{
+    //je commence par déterminer f_indice_x :
+    calcul_forces(tab_par);
+    for ( int i=0;i<nbx_particules;i++)
+    {
+        tab_par[i].vx = tab_par[i].vx + tab_par[i].fx*delta_t;
+        tab_par[i].vy  = tab_par[i].vy + tab_par[i].fy*delta_t;
+        tab_par[i].x =  tab_par[i].x + tab_par[i].vx*delta_t;
+        tab_par[i].y = tab_par[i].y + tab_par[i].vy*delta_t; 
+        
+
+       /*  printf("%d %15.4f %15.4f %15.4f\n",i,(pow(tab_par[i].vx,2)+pow(tab_par[i].vy,2)) \
+        ,tab_par[i].fx,tab_par[i].fy); */
     }
 
-    tab_par[indice].vx = tab_par[indice].vx + (f_x)*delta_t;
-    tab_par[indice].vy  = tab_par[indice].vy + (f_y)*delta_t;
-    tab_par[indice].x =  tab_par[indice].x + tab_par[indice].vx*delta_t;
-    tab_par[indice].y = tab_par[indice].y + tab_par[indice].vy*delta_t; 
+    
 }  
 
 int main() 
@@ -107,9 +138,21 @@ int main()
     //srand(time(NULL)); // pour les nombres aleatoires
     
     Particule tab_particules [nbx_particules];
-    initPoint(&tab_particules[0],1.4,2,6,9);
-    initPoint(&tab_particules[1],1.2,1.1,2,1);
+
+    initPoint(&tab_particules[0], 2, 2.3,  4,  3);
+
+    initPoint(&tab_particules[1], 1.5,  3,  3,  2);
+
+    initPoint(&tab_particules[2],  1,  1.5,  4,  5);
+
+    initPoint(&tab_particules[3],  1.7,  1.6,  2,  3);
+
+    initPoint(&tab_particules[4],  2.4,  3,  3,  5);
+    
     char command[500];
+
+    double t=0.0;
+    
     FILE *out;
 
      int i,j;
@@ -119,30 +162,25 @@ int main()
         //printf("%f\n",tab_particules[0].vx);
     }
  */
-    out = fopen ("Ep_Lennard_Jones.txt", "w");
-    for (j=0;j<20000;j++)
+    out = fopen ("Etot.txt", "w");
+    for (j=0;j<5000;j++)
     {
-        for ( i=0;i<nbx_particules;i++)
-        {   
-            resolution(tab_particules, i);
-        }
-        printf("%.10f\n",Energie_totale(tab_particules)); //verification energie totale
-        fprintf (out, "%22.8g  %22.8g \n", \
-        distance(&tab_particules[0],&tab_particules[1]),\
-        Potentiel_LJ(&tab_particules[0],&tab_particules[1]));
+        t+=delta_t;
+        resolution(tab_particules);
+        //printf("%.10f\n",Energie_totale(tab_particules)); //verification energie totale
+        fprintf (out, "%22.8g  %22.8g \n",t,\
+        Energie_totale(tab_particules));//Potentiel_LJ(&tab_particules[0],&tab_particules[1]));
     }
     fclose (out);
     //sprintf (command, "gnuplot -p -e \"plot 'potentiel.txt' w l , \" ");
    sprintf(command, 
-        "gnuplot -p -e \"set title 'Simulation Potentiel Lennard Jones' font ',16'; \
+        "gnuplot -p -e \"set title 'Etot_{12}' font ',16'; \
         set xlabel 'r/d'; \
         set ylabel 'E/E0'; \
-        set xrange [0.7:3]; \
-        set yrange [-1.5:3.5]; \
         set grid;\
-        plot 'Ep_Lennard_Jones.txt' w l\"");
+        plot 'Etot.txt' w l\"");
 
-    //system (command);
+    system (command);
 
     return 0;
 }
