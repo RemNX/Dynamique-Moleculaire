72211bb0a9114bef86321f1688168d96491260e6
Manon Nolot <150124061+Kurolycka@users.noreply.github.com>
2024-09-23 18:05:08 +0200

    manon manon manon

--- loi.py
+++ loi.py
@@ -5,16 +5,34 @@ import numpy as np
 #Je prends le minimum comme temps de référence t* et ce t* est à 0.00047
 
 #Enregistrer les fichiers au même endroit que le programme py : 
-nom_fichiers = ['Etot17.txt', 'Etot16.txt', 'Etot15.txt', 'Etot14.txt', 'Etot13.txt', 'Etot12.txt', 'Etot11.txt', 'Etot10.txt', 'Etot.txt', 'Etot2.txt', 'Etot3.txt', 'Etot4.txt', 'Etot5.txt', 'Etot6.txt', 'Etot7.txt', 'Etot8.txt', 'Etot9.txt', 'Etot18.txt', 
-                'Etot19.txt', 'Etot20.txt', 'Etot21.txt', 'Etot22.txt', 'Etot23.txt', 'Etot24.txt', 'Etot25.txt',  'Etot26.txt',  'Etot27.txt']
+nom_fichiers = []
+pas_de_temps=[]
+
+for i in range(-5,-3):
+    for j in np.arange(1,9.1,0.1):
+        j_rounded = round(j, 1)
+        nom_fichiers.append(f'Etot_{j_rounded}e{i}.txt')
+        pas_de_temps.append(float(f'{j_rounded}e{i}'))
+
+data_ref=[]
+temps_ref=[]
+
+with open('Etot_8.0e-5.txt','r') as file :
+    for line in file :
+        left,right = line.split(':')
+        data_ref.append(float(right.strip()))
+        temps_ref.append(float(left.strip()))
+    maxi = max(data_ref)
+    for h in range(0,len(data_ref)):
+        if data_ref[h] == maxi :
+            indice=h
+
+E0 = data_ref[0]
+temps_ref_final=temps_ref[indice]
+
 liste_temps=[]
-pas_de_temps=[10e-14,10e-13,10e-12,10e-11,10e-10,10e-9,10e-8,10e-7,10e-6, 10e-5, 10e-4, 10e-3, 10e-2, 10e-1, 10e0, 10e1, 10e2, 10e3, 10e4, 10e5, 10e6, 10e7, 10e8, 10e9, 10e10,10e20, 10e30]
 data = []
 liste_E_t_ref=[]
-
-#récupéré avec un min pour un pas de 10e-5 :
-temp_ref = 0.00047 
-E0=64559.103
 
 for i in nom_fichiers :
     with open(i, 'r') as file :
@@ -24,8 +42,13 @@ for i in nom_fichiers :
             data.append(float(right.strip()))
             liste_temps.append(float(left.strip()))
 
-        closest_index = min(range(len(liste_temps)), key=lambda i: abs(liste_temps[i] - temp_ref))
+        init = abs(liste_temps[0]-temps_ref_final)
+        for l in range(0,len(liste_temps)):
+            if abs(liste_temps[l]-temps_ref_final) <= init :
+                closest_index=l
+
         closest_value = data[closest_index]
+        print(closest_value)
         liste_E_t_ref.append(closest_value)
 
         data=[]
@@ -33,11 +56,11 @@ for i in nom_fichiers :
 
 liste_diff = [np.log(abs(i - E0)) for i in liste_E_t_ref]
 liste_diff_deltat =[np.log(j) for j in pas_de_temps]
-droite=[k for k in liste_diff_deltat]
-
+droite = [k for k in liste_diff_deltat]
 
 plt.plot( liste_diff_deltat, liste_diff, "o")
 plt.plot(liste_diff_deltat, droite)
 plt.xlabel("ln(δt)")
 plt.ylabel("ln(|ΔE(t*)|)")
+#plt.xlim(-9.5,-7)
 plt.show()
---
+++ main.cpp
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+//#define delta_t  10e-6
+double delta_t;
+#define nbx_particules   5
+
+/*Definition Particule avec x,y,vitesse selon x
+, vitesse selon y*/
+typedef struct
+{
+    double x;
+    double y;
+    double vx;
+    double vy;
+    double fx;
+    double fy;
+} Particule;
+
+/*Constructeur*/
+void initPoint(Particule *p, double x, double y, double vx, double vy)
+{
+    p->x = x;
+    p->y = y;
+    p->vx = vx;
+    p->vy = vy;
+}
+/*Calcul distance entre 2 particules*/
+double distance(Particule *p1, Particule *p2) {
+    return sqrt(pow(p2->x - p1->x, 2) + pow(p2->y - p1->y, 2));
+}
+
+/*Fonction qui genere des positions et vitesse
+aleatoires, pour une particule*/
+Particule genererParticule()
+{
+    Particule p;
+    p.x = (double)rand()/ RAND_MAX;
+    p.y = (double)rand() / RAND_MAX;
+    p.vx = (double)rand() / RAND_MAX;
+    p.vy = (double)rand() / RAND_MAX;
+    return p;
+}
+
+/*Fonction qui calcule la force selon x en Lennard Jones entre
+deux particules*/
+double force_selon_x(Particule *p2, Particule *p1)
+{
+    double rij = distance(p1, p2);
+    double terme_commun = 24*(pow(1/rij,8)-2*pow(1/rij,14));
+    return terme_commun*(p2->x-p1->x);
+}
+
+/*Fonction qui calcule la force selon y en Lennard Jones entre
+deux particules*/
+double force_selon_y(Particule *p2, Particule *p1)
+{
+    double rij = distance(p1, p2);
+    double terme_commun = 24*(pow(1/rij,8)-2*pow(1/rij,14));
+    return terme_commun*(p2->y-p1->y);
+}
+
+double Potentiel_LJ(Particule *p2, Particule *p1)
+{
+    double rij=distance(p1,p2);
+    double U=4*(pow((1/rij),12)-pow((1/rij),6));
+    return U;
+}
+
+double Energie_totale(Particule tab_par[])
+{
+    double E=0.0,U=0.0;
+
+    for (int i=0;i<nbx_particules;i++)
+    {
+        for (int j=i+1;j<nbx_particules;j++)
+        {
+
+            U+=Potentiel_LJ(&tab_par[j],&tab_par[i]);
+
+        }
+        E+=(pow(tab_par[i].vx,2)+pow(tab_par[i].vy,2))/2;
+
+    }
+    return U+E;
+}
+
+/*Fonction qui resoud les equations differentielles d'une particule
+designé par un "indice", et qui prend un tableau dans lequel sont
+stockées les particules, elle ne retourne rien car tout est modifié
+directement */
+void calcul_forces (Particule tab_par[])
+{
+    //je commence par déterminer f_indice_x :
+    double f_x;
+    double f_y;
+
+    for ( int i=0;i<nbx_particules;i++)
+    {
+        f_x=0.0;
+        f_y=0.0;
+        for ( int j=0;j<nbx_particules;j++)
+        {
+            if (j != i)
+            {
+                f_x += force_selon_x(&tab_par[j],&tab_par[i]);
+                f_y += force_selon_y(&tab_par[j],&tab_par[i]);
+            }
+        }
+
+        tab_par[i].fx=f_x;
+        tab_par[i].fy=f_y;
+    }
+
+}
+
+void resolution(Particule tab_par[])
+{
+    //je commence par déterminer f_indice_x :
+    calcul_forces(tab_par);
+    for ( int i=0;i<nbx_particules;i++)
+    {
+        tab_par[i].vx = tab_par[i].vx + tab_par[i].fx*delta_t;
+        tab_par[i].vy  = tab_par[i].vy + tab_par[i].fy*delta_t;
+        tab_par[i].x =  tab_par[i].x + tab_par[i].vx*delta_t;
+        tab_par[i].y = tab_par[i].y + tab_par[i].vy*delta_t;
+
+
+       /*  printf("%d %15.4f %15.4f %15.4f\n",i,(pow(tab_par[i].vx,2)+pow(tab_par[i].vy,2)) \
+        ,tab_par[i].fx,tab_par[i].fy); */
+    }
+
+
+}
+
+int main()
+{
+    char nom_fichier[20];
+    for (int h=-5; h<=-4;h++){
+        for (float k=1;k<=9;k+=0.1){
+        sprintf(nom_fichier,"Etot_%.1fe%d.txt",k,h);
+        delta_t = k*pow(10,h);
+
+        //srand(time(NULL)); // pour les nombres aleatoires
+
+        Particule tab_particules [nbx_particules];
+
+            initPoint(&tab_particules[0], 1 , 1,  10.4,  10);
+
+            initPoint(&tab_particules[1], 0.8 ,  4.5,  50,  10.6);
+
+            initPoint(&tab_particules[2],  0,  2,  30,  10);
+
+            initPoint(&tab_particules[3],  1.3 ,  0,  40, 20);
+
+            initPoint(&tab_particules[4],  1.2 ,  3.1,  40,  0);
+
+        char command[500];
+
+        double t=0.0;
+
+        FILE *out;
+
+        int i,j;
+    /*  for ( i=0;i<10;i++)
+        {
+            tab_particules[i]=genererParticule();
+            //printf("%f\n",tab_particules[0].vx);
+        }
+    */
+        //out = fopen ("Etot.txt", "w");
+        out = fopen (nom_fichier, "w");
+        for (j=0;j<5000;j++)
+        {
+            t+=delta_t;
+            resolution(tab_particules);
+            //printf("%.10f\n",Energie_totale(tab_particules)); //verification energie totale
+            fprintf (out, "%22.8g : %22.8g \n",t,\
+            Energie_totale(tab_particules));//Potentiel_LJ(&tab_particules[0],&tab_particules[1]));
+        }
+        fclose (out);
+        //sprintf (command, "gnuplot -p -e \"plot 'potentiel.txt' w l , \" ");
+    /* sprintf(command,
+            "gnuplot -p -e \"set title 'Etot_{12}' font ',16'; \
+            set xlabel 'r/d'; \
+            set ylabel 'E/E0'; \
+            set grid;\
+            plot Etot_5e-6' w l\"");
+
+        system (command); */
+
+        }
+    }
+    return 0;
+}
